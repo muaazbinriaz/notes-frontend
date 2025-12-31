80926dd0825326f95311128bd45c8dcba0fc45ce
muaaz <muaazbinriaz2000@gmail.com>
2025-12-31 21:23:03 +0500

    updated

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,6 @@
 export const getAuthHeader = () => {
   const token = localStorage.getItem("token");
-  return { Authorization: `Bearer ${token}` };
+  return {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 };
