12e08b948bf9d1d18bf915e0ac666e16ebede2c6
muaaz <muaazbinriaz2000@gmail.com>
2026-01-29 20:38:07 +0500

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -175,6 +175,41 @@ function Login() {
               Login
             </h1>
             <form className="space-y-4" onSubmit={handleLogin}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={loginInfo.email}
+                  autoFocus
+                  placeholder="Enter your email..."
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={loginInfo.password}
+                  placeholder="Enter your password..."
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
+                />
+              </div>
+
               <button
                 type="submit"
                 className="w-full bg-[#39393a] cursor-pointer hover:bg-[#222223] text-white font-semibold py-2 rounded-md transition duration-200"
